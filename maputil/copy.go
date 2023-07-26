6e028d48ca0ce7648a889ec913f62c0c5fc7945f
revolution1 <revol.cai@gmail.com>
2023-07-26 22:47:21 +0800

    maputil copy

--- maputil/copy.go
+++ maputil/copy.go
@@ -1,5 +1,15 @@
 package maputil
 
+func Copy[T comparable, P any](dst *map[T]P, src map[T]P) {
+	*dst = make(map[T]P, len(src))
+	if len(src) == 0 {
+		return
+	}
+	for k, v := range src {
+		(*dst)[k] = v
+	}
+}
+
 func Merge[T comparable, P any](dst *map[T]P, src map[T]P) {
 	if len(src) == 0 {
 		return
@@ -13,6 +23,7 @@ func Merge[T comparable, P any](dst *map[T]P, src map[T]P) {
 		}
 	}
 }
+
 func MergeOverwrite[T comparable, P any](dst *map[T]P, src map[T]P) {
 	if len(src) == 0 {
 		return
